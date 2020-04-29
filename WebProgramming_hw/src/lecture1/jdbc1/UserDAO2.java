775ad596882c2a5eac3c4a1b8faa823336ced9e4
seyeonn <ansse17@gmail.com>
2020-04-29 17:57:42 +0900

    jdbc 이름으로 조회 hw

--- WebProgramming_hw/src/lecture1/jdbc1/UserDAO2.java
+++ WebProgramming_hw/src/lecture1/jdbc1/UserDAO2.java
@@ -3,27 +3,12 @@ package lecture1.jdbc1;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import lecture1.DB;
 
 public class UserDAO2 {
-
-    private static User getUserFrom(ResultSet resultSet) throws SQLException {
-        User user = new User();
-        user.setId(resultSet.getInt("id"));
-        user.setUserid(resultSet.getString("userid"));
-        user.setPassword(resultSet.getString("password"));
-        user.setName(resultSet.getString("name"));
-        user.setEmail(resultSet.getString("email"));
-        user.setDepartmentId(resultSet.getInt("departmentId"));
-        user.setEnabled(resultSet.getBoolean("enabled"));
-        user.setUserType(resultSet.getString("userType"));
-        user.setDepartmentName(resultSet.getString("departmentName"));
-        return user;
-    }
 
     public static List<User> findAll() throws Exception {
         String sql = "SELECT u.*, d.departmentName " +
@@ -32,8 +17,19 @@ public class UserDAO2 {
              PreparedStatement statement = connection.prepareStatement(sql);
              ResultSet resultSet = statement.executeQuery()) {
             ArrayList<User> list = new ArrayList<User>();
-            while (resultSet.next())
-                list.add(getUserFrom(resultSet));
+            while (resultSet.next()) {
+            	User user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setUserid(resultSet.getString("userid"));
+                user.setPassword(resultSet.getString("password"));
+                user.setName(resultSet.getString("name"));
+                user.setEmail(resultSet.getString("email"));
+                user.setDepartmentId(resultSet.getInt("departmentId"));
+                user.setEnabled(resultSet.getBoolean("enabled"));
+                user.setUserType(resultSet.getString("userType"));
+                user.setDepartmentName(resultSet.getString("departmentName"));
+                list.add(user);
+            }
             return list;
         }
     }
@@ -47,8 +43,19 @@ public class UserDAO2 {
             statement.setString(1, name + "%");
             try (ResultSet resultSet = statement.executeQuery()) {
                 ArrayList<User> list = new ArrayList<User>();
-                while (resultSet.next())
-                    list.add(getUserFrom(resultSet));
+                while (resultSet.next()) {
+                	User user = new User();
+                    user.setId(resultSet.getInt("id"));
+                    user.setUserid(resultSet.getString("userid"));
+                    user.setPassword(resultSet.getString("password"));
+                    user.setName(resultSet.getString("name"));
+                    user.setEmail(resultSet.getString("email"));
+                    user.setDepartmentId(resultSet.getInt("departmentId"));
+                    user.setEnabled(resultSet.getBoolean("enabled"));
+                    user.setUserType(resultSet.getString("userType"));
+                    user.setDepartmentName(resultSet.getString("departmentName"));
+                    list.add(user);
+                }
                 return list;
             }
         }
