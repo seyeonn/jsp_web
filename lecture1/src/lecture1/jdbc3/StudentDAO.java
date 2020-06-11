3ef0329d6bac432dc3335a645532bc4bb7ab70ca
seyeonn <ansse17@gmail.com>
2020-06-11 20:53:45 +0900

    jdbc4 실습

--- lecture1/src/lecture1/jdbc3/StudentDAO.java
+++ lecture1/src/lecture1/jdbc3/StudentDAO.java
@@ -35,6 +35,34 @@ public class StudentDAO {
         }
     }
 
+    public static List<Student> findAll(int currentPage, int pageSize) throws Exception {
+        String sql = "SELECT s.*, d.departmentName" +
+                     " FROM student s LEFT JOIN department d ON s.departmentId = d.id" +
+                     " LIMIT ?, ?";
+        try (Connection connection = DB.getConnection("student1");
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, (currentPage - 1) * pageSize);
+            statement.setInt(2, pageSize);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                ArrayList<Student> list = new ArrayList<Student>();
+                while (resultSet.next())
+                    list.add(getStudentFrom(resultSet));
+                return list;
+            }
+        }
+    }
+
+    public static int count() throws Exception {
+        String sql = "SELECT COUNT(*) FROM student";
+        try (Connection connection = DB.getConnection("student1");
+             PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next())
+                    return resultSet.getInt(1);
+        }
+        return 0;
+    }
+
     public static Student findByStudentNumber(String studentNumber) throws Exception {
         String sql = "SELECT s.*, d.departmentName" +
                      " FROM student s LEFT JOIN department d ON s.departmentId = d.id" +
